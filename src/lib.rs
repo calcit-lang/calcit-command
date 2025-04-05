7ce86bf048df64a18c498d97508e3fce0d295106
tiye <jiyinyiyong@gmail.com>
2025-04-06 02:47:34 +0800

    a demo of running command

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use cirru_edn::Edn;
 use std::path::Path;
+use std::process::Command;
 
 #[no_mangle]
 pub fn abi_version() -> String {
@@ -18,3 +19,36 @@ pub fn path_exists(args: Vec<Edn>) -> Result<Edn, String> {
     Err(format!("path-exists? expected 1 arg, got {:?}", args))
   }
 }
+
+
+/// simple command to run a command, without options
+#[no_mangle]
+pub fn run_command(args: Vec<Edn>) -> Result<Edn, String> {
+  let mut xs = vec![];
+  for arg in args.iter() {
+    if let Edn::Str(name) = arg {
+      xs.push((*name).to_owned());
+    } else {
+      return Err(format!("run-command expected 1 filename, got {:?}", args))
+    }
+  }
+  if xs.is_empty()  {
+    Err(format!("run-command expected at least 1 arg, got {:?}", args))
+  } else {
+    let name = xs.remove(0);
+    let mut command = Command::new(&(*name));
+    for arg in xs.iter() {
+      command.arg(&*(*arg).to_owned());
+    }
+
+    let output = command.output()
+      .expect("failed to execute process");
+    if output.status.success() {
+
+      Ok(Edn::str(String::from_utf8_lossy(&output.stdout).to_string()))
+    } else {
+      Err(format!("run-command failed: {}", String::from_utf8_lossy(&output.stderr)))
+    }
+  }
+}
+
